d77b6496dfd081ace9ebcbc29227fe22dc1843e3
Raphael Lopes <ph.campanario07@gmail.com>
2024-05-06 10:57:19 -0300

    add pagination

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,10 +5,45 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [totalPeople, setTotalPeople] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [disablePrev, setDisablePrev] = useState(true);
+  const [disableNext, setDisableNext] = useState(true);
+
   const fetchData = async () => {
     const response = await axios.get("https://swapi.dev/api/people");
-    console.log(response.data);
     setData(response.data.results);
+    setTotalPeople(response.data.count);
+    if (response.data.next) {
+      setDisableNext(false);
+    } else {
+      setDisableNext(true);
+    }
+
+    if (response.data.previous) {
+      setDisablePrev(false);
+    } else {
+      setDisablePrev(true);
+    }
+  };
+
+  const fetchPage = async (page: number) => {
+    const response = await axios.get(
+      "https://swapi.dev/api/people?page=" + page,
+    );
+    setData(response.data.results);
+
+    if (response.data.next) {
+      setDisableNext(false);
+    } else {
+      setDisableNext(true);
+    }
+
+    if (response.data.previous) {
+      setDisablePrev(false);
+    } else {
+      setDisablePrev(true);
+    }
   };
 
   useEffect(() => {
@@ -22,6 +57,14 @@ export default function Home() {
       </header>
       <div className="max-w-3xl m-auto">
         <h2 className="text-2xl font-bold mt-4">Star Wars Characters</h2>
+        <nav>
+          <p>
+            Total people: <strong>{totalPeople}</strong>
+          </p>
+          <p>
+            Current page: <strong>{currentPage}</strong>
+          </p>
+        </nav>
         <ul>
           {data.map((item: any) => (
             <li key={item.name} className="border-b last:border-0 py-4">
@@ -29,6 +72,28 @@ export default function Home() {
             </li>
           ))}
         </ul>
+        <nav className="flex items-center justify-center space-x-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={() => {
+              setCurrentPage((prev) => prev - 1);
+              fetchPage(currentPage - 1);
+            }}
+            disabled={disablePrev}
+          >
+            Previous
+          </button>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={() => {
+              setCurrentPage((prev) => prev + 1);
+              fetchPage(currentPage + 1);
+            }}
+            disabled={disableNext}
+          >
+            Next
+          </button>
+        </nav>
       </div>
     </main>
   );
